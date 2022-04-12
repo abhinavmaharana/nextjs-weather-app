3cb5083105c6f5d5b5fee08853abec49671d1222
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-04-12 22:55:46 +0530

    🚀 Final Deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
 import "../styles/main.scss";
+import Router from 'next/router';
+
+import ProgressBar from '@badrap/bar-of-progress';
+
 import type { AppProps } from 'next/app'
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+
+Router.events.on('routeChangeComplete', progress.finish);
+
+Router.events.on('routeChangeError', progress.finish);
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
